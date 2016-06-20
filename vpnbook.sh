23a43d4ac093b772e7a07e1f549bf4fe4b630e20
enigma <auxiliary42@gmail.com>
2016-06-20 15:29:43 -0400

    Create vpnbook.sh

---
+++ vpnbook.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#This script prints the VPNBook public username and password
+
+wget -q http://www.vpnbook.com/freevpn
+cat freevpn | grep Username: | sed -e '1d' | cut -d ">" -f 3 | cut -d "<" -f 1
+cat freevpn | grep Password: | sed -e '1d' | cut -d ">" -f 3 | cut -d "<" -f 1
+rm freevpn
