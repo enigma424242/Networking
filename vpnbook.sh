b346e71cc2b43029fb116e1f54c7b29888be0ba2
enigma <auxiliary42@gmail.com>
2021-09-23 11:57:23 -0400

    Update vpnbook.sh

--- vpnbook.sh
+++ vpnbook.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# This script prints the VPNBook public username and password
+# Prints the VPNBook public username and password to the console prompt
 
 wget -q http://www.vpnbook.com/freevpn
 cat freevpn | grep Username: | sed -e '1d' | cut -d ">" -f 3 | cut -d "<" -f 1
